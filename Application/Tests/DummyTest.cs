e350213f8a4c554a5282b507b7e0c771b91c2b76
Luiz Felipe <lfmachadodasilva@gmail.com>
2017-10-19 17:18:10 +0100

    #41 Created unit-test project for infra and app.

---
+++ Application/Tests/DummyTest.cs
@@ -0,0 +1,20 @@
+﻿/* 
+*   Project: MyBaseSolution
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace MyExpenses.Domain.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void DummyTestWhenIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Infrastructure/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyExpenses.Infrastructure.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyExpenses.Infrastructure.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfce1d1a-5b20-41ac-a156-a8a6076d3276")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
