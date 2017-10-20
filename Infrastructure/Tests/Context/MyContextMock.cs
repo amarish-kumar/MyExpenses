b713ead6a3d099be8bef73a9576aae806decb1e1
Luiz Felipe <lfmachadodasilva@gmail.com>
2017-10-20 11:57:07 +0100

    Created mock for context

--- Infrastructure/Tests/Context/MyContextMock.cs
+++ Infrastructure/Tests/Context/MyContextMock.cs
@@ -6,9 +6,25 @@
 
 namespace MyExpenses.Infrastructure.Tests.Context
 {
+    using System.Data.Entity;
+
+    using MyExpenses.Domain.Models;
     using MyExpenses.Infrastructure.Context;
 
     public class MyContextMock : IMyContext
     {
+        public IDbSet<Expense> Expenses { get; set; }
+
+        public IDbSet<Tag> Tags { get; set; }
+
+        DbSet<TEntity> IMyContext.Set<TEntity>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Infrastructure/Tests/DummyTest.cs
+++ Infrastructure/Tests/DummyTest.cs
@@ -6,15 +6,6 @@
 
 namespace MyExpenses.Infrastructure.Tests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
-    using Moq;
-
-    using MyExpenses.Domain.Models;
-    using MyExpenses.Infrastructure.Repositories;
-
     using NUnit.Framework;
 
     [TestFixture]
@@ -23,21 +14,7 @@ namespace MyExpenses.Infrastructure.Tests
         [Test]
         public void DummyTestWhenIsTrue()
         {
-            //arrange
-            var mockMyViewModel = new Mock<ExpensesRepo> { CallBase = true };
-            mockMyViewModel.Setup(x => x.Get(null)).Returns(new List<Expense>
-                                                                      {
-                                                                          new Expense
-                                                                              {
-                                                                                  Id = 1,
-                                                                                  Name = "Test Name",
-                                                                                  Value = 2,
-                                                                                  Date = new DateTime()
-                                                                              }
-                                                                      });
-
-            var list = mockMyViewModel.Object.Get();
-            Assert.IsTrue(list.Any());
+            Assert.True(true);
         }
     }
 }
