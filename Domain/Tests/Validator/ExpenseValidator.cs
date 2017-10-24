dff7209a8bff0e8ee0a8faf4678770e04e6e4d3d
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2017-10-24 19:27:53 +0100

    #12 unit-test for expense validator

---
+++ Domain/Tests/Validator/ExpenseValidator.cs
@@ -0,0 +1,68 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace MyExpenses.Domain.Tests.Validator
+{
+    using MyExpenses.Domain.Models;
+    using MyExpenses.Util.Results;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExpenseValidator
+    {
+        private Expense _expense;
+
+        [SetUp]
+        public void Setup()
+        {
+            _expense = new Expense
+            {
+                Id = 1,
+                Name = "Expense1",
+                Value = 1,
+                Date = new System.DateTime()
+            };
+        }
+
+        [Test]
+        public void TestExpenseValidator_Validate_Ok()
+        {
+            MyResults results = _expense.Validate();
+
+            Assert.True(results.Type == MyResultsType.Ok);
+        }
+
+        [Test]
+        public void TestExpenseValidator_IdIsInvalid_Validate_ResultError()
+        {
+            _expense.Id = -1;
+
+            MyResults results = _expense.Validate();
+
+            Assert.True(results.Type == MyResultsType.Error);
+        }
+
+        [Test]
+        public void TestExpenseValidator_NameIsEmtpy_Validate_ResultError()
+        {
+            _expense.Name = string.Empty;
+
+            MyResults results = _expense.Validate();
+
+            Assert.True(results.Type == MyResultsType.Error);
+        }
+
+        [Test]
+        public void TestExpenseValidator_ValueIsInvalid_Validate_ResultError()
+        {
+            _expense.Value = -1;
+
+            MyResults results = _expense.Validate();
+
+            Assert.True(results.Type == MyResultsType.Error);
+        }
+    }
+}
