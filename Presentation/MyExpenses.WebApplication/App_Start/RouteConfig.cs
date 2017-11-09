965db1ff5ab51ce89d6987086e0d388336b005a1
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2017-11-09 19:51:43 +0000

    Created view to list all expenses and create new expense #50 #52 #54

--- Presentation/MyExpenses.WebApplication/App_Start/RouteConfig.cs
+++ Presentation/MyExpenses.WebApplication/App_Start/RouteConfig.cs
@@ -13,10 +13,12 @@ namespace MyExpenses.WebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Expenses", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
