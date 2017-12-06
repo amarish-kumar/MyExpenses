9c29eea57a908a3963fa3cec23b3ec9a898918bd
Luiz Felipe <lfmachadodasilva@gmail.com>
2017-12-06 17:44:58 +0000

    #30 Created project for dotnet console

---
+++ Presentation.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace Presentation.Console
+{
+    using System;
+
+    using MyExpenses.Application.DataTransferObject;
+    using MyExpenses.Application.Interfaces.Services;
+    using MyExpenses.Application.Modules;
+    using MyExpenses.Infrastructure.Modules;
+    using MyExpenses.Util.IoC;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loads only necessary modules
+            MyKernelService.Init();
+            MyApplicationModule.Init();
+            MyInfrastructureModule.Init();
+
+            Console.WriteLine("Expenses:");
+
+            var appService = MyKernelService.GetInstance<IExpensesAppService>();
+            foreach (ExpenseDto expenseDto in appService.GetAll())
+            {
+                Console.WriteLine($"{expenseDto.Name}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
