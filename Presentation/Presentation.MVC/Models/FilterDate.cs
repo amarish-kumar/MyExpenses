6cf8292c718f6041a9047e422d7af0cb45f5829a
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2017-12-19 18:50:31 +0000

    Created class to handle filter date #89 #90

---
+++ Presentation/Presentation.MVC/Models/FilterDate.cs
@@ -0,0 +1,55 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace Presentation.MVC.Models
+{
+    using System;
+
+    public class FilterDate
+    {
+        private FilterDateType _filterDateType { get; set; }
+
+        public FilterDateType FilterDateType
+        {
+            get => _filterDateType;
+
+            set
+            {
+                _filterDateType = value;
+
+                switch(_filterDateType)
+                {
+                    case FilterDateType.CurrentMonth:
+                        StartDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                        EndDate = DateTime.Today;
+                        break;
+                    case FilterDateType.LastMonth:
+                        StartDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-1);
+                        EndDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddDays(-1);
+                        break;
+                }
+            }
+        }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public FilterDate()
+        {
+            FilterDateType = FilterDateType.CurrentMonth;
+            StartDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            EndDate = DateTime.Today;
+        }
+    }
+
+    public enum FilterDateType
+    {
+        CurrentMonth,
+        LastMonth,
+        Custom
+    }
+}
