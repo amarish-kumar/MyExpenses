97bf3d09ab2548ef0d3f3fd70402a196afb53463
Luiz Felipe <lfmachadodasilva@gmail.com>
2018-03-12 16:01:58 +0000

    Removing all to test new idea

--- Infrastructure/Sql/Sqlite/populateTables.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT into Expenses ('Name', 'Value', 'Data') VALUES ('Expense1', 1, '2017-10-10');
-INSERT into Expenses ('Name', 'Value', 'Data') VALUES ('Expense2', 2, '2017-10-10');
-
-INSERT into Tags ('Name') VALUES ('Tag1');
-INSERT into Tags ('Name') VALUES ('Tag2');
-
-INSERT into Expenses_Tags ('ExpenseId', 'TagId') VALUES (1, 1);
-INSERT into Expenses_Tags ('ExpenseId', 'TagId') VALUES (2, 2);
