a50e13d6b416c4457ec7b90b67a2beb7a5752d0f
Luiz Felipe <lfmachadodasilva@gmail.com>
2018-04-27 14:45:56 +0100

    Configure to run on localhost

--- Presentation/WebApplicationMVC/Program.cs
+++ Presentation/WebApplicationMVC/Program.cs
@@ -6,14 +6,42 @@
 
 namespace MyExpenses.WebApplicationMVC
 {
+    using System.IO;
+
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            if (args.Length > 0)
+            {
+                // use this to allow command line parameters in the config
+                var configuration = new ConfigurationBuilder()
+                    .AddCommandLine(args)
+                    .Build();
+
+                var hostUrl = configuration["hosturl"];
+                if (string.IsNullOrEmpty(hostUrl))
+                    hostUrl = "http://0.0.0.0:6000";
+
+                var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseUrls(hostUrl)   // <!-- this 
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .UseConfiguration(configuration)
+                    .Build();
+
+                host.Run();
+            }
+            else
+            {
+                BuildWebHost(args).Run();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
