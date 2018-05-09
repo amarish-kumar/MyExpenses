e087efbecb448ca3a8adedd80ad274c58352c91d
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2018-05-09 21:35:21 +0100

    #125 Start adapter application unit-test

---
+++ Application/Test/ServicesTest.cs
@@ -0,0 +1,32 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace ApplicationTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ServicesTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void Dummy()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
