219ade9abf90d8a87af3b696d0529502519a589c
Luiz Felipe <lfmachadodasilva@gmail.com>
2018-05-22 10:57:39 +0100

    Removed some codacy warnings

--- Presentation/WebApplicationMVC/Program.cs
+++ Presentation/WebApplicationMVC/Program.cs
@@ -18,10 +18,6 @@ namespace MyExpenses.WebApplicationMVC
     {
         public static void Main(string[] args)
         {
-            //CultureInfo cultureInfo = CultureInfo.CreateSpecificCulture("en-US");
-            //Thread.CurrentThread.CurrentCulture = cultureInfo;
-            //Thread.CurrentThread.CurrentUICulture = cultureInfo;
-
             if (args.Length > 0)
             {
                 // use this to allow command line parameters in the config
