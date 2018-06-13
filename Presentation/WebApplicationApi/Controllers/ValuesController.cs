1537b40fd10988ee874d621b989435880b78cc7f
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2018-06-13 19:37:49 +0100

    Created a project for web api

---
+++ Presentation/WebApplicationApi/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace WebApplicationApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    using MyExpenses.Application.Interfaces.Services;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IExpenseAppService _expenseAppService;
+
+        public ValuesController(IExpenseAppService expenseAppService)
+        {
+            _expenseAppService = expenseAppService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
