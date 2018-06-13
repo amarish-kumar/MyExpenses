725e65da06433ff28b9444dd2a1bc04f83e79965
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2018-06-13 22:28:18 +0100

    Change the organization of all others layers

---
+++ Util/Source/MyDate.cs
@@ -0,0 +1,61 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace MyExpenses.Util
+{
+    using System;
+
+    public static class MyDate
+    {
+        public static DateTime GetStartDateTime(int month, int year)
+        {
+            int m = month <= 0 ? DateTime.Today.Month : month;
+            int y = year <= 0 ? DateTime.Today.Year : year;
+
+            return new DateTime(y, m, 1);
+        }
+
+        public static DateTime GetStartDateTime(DateTime date)
+        {
+            int m = date.Month <= 0 ? DateTime.Today.Month : date.Month;
+            int y = date.Year <= 0 ? DateTime.Today.Year : date.Year;
+
+            return new DateTime(y, m, 1);
+        }
+
+        public static DateTime GetEndDateTime(int month, int year)
+        {
+            int m = month <= 0 ? DateTime.Today.Month : month;
+            int y = year <= 0 ? DateTime.Today.Year : year;
+
+            return new DateTime(y, m, 1).AddMonths(1).AddDays(-1);
+        }
+
+        public static DateTime GetEndDateTime(DateTime date)
+        {
+            int m = date.Month <= 0 ? DateTime.Today.Month : date.Month;
+            int y = date.Year <= 0 ? DateTime.Today.Year : date.Year;
+
+            return new DateTime(y, m, 1).AddMonths(1).AddDays(-1);
+        }
+
+        public static DateTime GetStartLastMonth(int month, int year)
+        {
+            int m = month - 1 < 1 ? 12 : month - 1;
+            int y = month - 1 < 1 ? year - 1 : year;
+
+            return new DateTime(y, m, 1);
+        }
+
+        public static DateTime GetEndLastMonth(int month, int year)
+        {
+            int m = month - 1 < 1 ? 12 : month - 1;
+            int y = month - 1 < 1 ? year - 1 : year;
+
+            return new DateTime(y, m, 1).AddMonths(1).AddDays(-1);
+        }
+    }
+}
