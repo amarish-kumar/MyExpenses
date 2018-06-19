f93b88944c8092eb60f8be6651cf6894483a4686
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2018-06-19 21:48:03 +0100

    Add missing comment

--- Application/Source/AutoMapper/ExpenseProfile.cs
+++ Application/Source/AutoMapper/ExpenseProfile.cs
@@ -15,12 +15,17 @@ namespace MyExpenses.Application.AutoMapper
     {
         public ExpenseProfile()
         {
+            // if the object id is small than 1, map to null
+
             CreateMap<Expense, ExpenseDto>()
+                
                 .ForMember(x => x.LabelId, opt => { opt.MapFrom(src => src.LabelId < 1 ? null : src.LabelId); })
                 .ForMember(x => x.Label, opt => { opt.MapFrom(src => Mapper.Map<LabelDto>(src.Label)); })
                 .ForMember(x => x.PaymentId, opt => { opt.MapFrom(src => src.PaymentId < 1 ? null : src.PaymentId); })
                 .ForMember(x => x.Payment, opt => { opt.MapFrom(src => Mapper.Map<PaymentDto>(src.Payment)); })
+
                 .ReverseMap()
+
                 .ForMember(x => x.LabelId, opt => { opt.MapFrom(src => src.LabelId < 1 ? null : src.LabelId); })
                 .ForMember(x => x.Label, opt => { opt.MapFrom(src => Mapper.Map<Label>(src.Label)); })
                 .ForMember(x => x.PaymentId, opt => { opt.MapFrom(src => src.PaymentId < 1 ? null : src.PaymentId); })
