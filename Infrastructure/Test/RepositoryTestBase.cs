ec1139e99dd40e56513929b5305847b36a63ad1a
Luiz Felipe <lfmachadodasilva@gmail.com>
2018-06-20 16:42:15 +0100

    Repositories - unit-tests #104

--- Infrastructure/Test/RepositoryTestBase.cs
+++ Infrastructure/Test/RepositoryTestBase.cs
@@ -32,5 +32,36 @@ namespace MyExpenses.InfrastructureTest
 
             Assert.IsTrue(all.Any());
         }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(3)]
+        [DataRow(5)]
+        [DataRow(7)]
+        public void RemoveAValidObject(long id)
+        {
+            UnitOfWork.BeginTransaction();
+            bool removed = Repository.Remove(id);
+            if (removed)
+                UnitOfWork.Commit();
+
+            Assert.IsTrue(removed);
+            Assert.IsNull(Repository.GetById(id));
+        }
+
+        [TestMethod]
+        [DataRow(100)]
+        [DataRow(1000)]
+        [DataRow(10000)]
+        public void RemoveAnInvalidObject(long id)
+        {
+            UnitOfWork.BeginTransaction();
+            bool removed = Repository.Remove(id);
+            if (removed)
+                UnitOfWork.Commit();
+
+            Assert.IsFalse(removed);
+            Assert.IsNull(Repository.GetById(id));
+        }
     }
 }
