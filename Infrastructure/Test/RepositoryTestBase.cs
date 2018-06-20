5552e41ad1316e1ad95b42823468ed7681158afb
Luiz Felipe <lfmachadodasilva@gmail.com>
2018-06-20 15:03:30 +0100

    Repositories - unit-tests #104

---
+++ Infrastructure/Test/RepositoryTestBase.cs
@@ -0,0 +1,29 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace MyExpenses.InfrastructureTest
+{
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using MyExpenses.Domain.Interfaces;
+
+    public abstract class RepositoryTestBase<TModel, TRepository> : InfrastructureTestBase where TModel : IModel where TRepository : IService<TModel>
+    {
+        protected TRepository Repository;
+
+        protected IUnitOfWork UnitOfWork;
+
+        [TestMethod]
+        public void InitAndFill()
+        {
+            var all = Repository.Get();
+
+            Assert.IsTrue(all.Any());
+        }
+    }
+}
